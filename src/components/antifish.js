16699d3cf62ecd72b84167f2fb98c4ee45dabc29
Mirko Westermeier <mirko.westermeier@uni-muenster.de>
2025-11-28 17:51:40 +0100

    Revert "Path continuation in 404 case"
    This reverts commit 46aa360e3678e2fa693f7cdabe9611f5295ea05d.

--- src/components/antifish.js
+++ src/components/antifish.js
@@ -6,19 +6,11 @@ import './about.js'
 export class AntiFish extends LitElement {
   constructor() {
     super()
-
-    const params = new URLSearchParams(window.location.search)
-    const spaPath = params.get('path')
-    if (spaPath) {
-      window.history.replaceState({}, '', spaPath)
-    }
-
     this.router = new Router(this, [
       { path: '/', render: () => html`<af-home></af-home>` },
       { path: '/about', render: () => html`<af-about></af-about>` },
     ])
   }
-
   render() {
     return html`${this.router.outlet()}`
   }
